c0c934d5eb3aa060fb61a0d0e267416d967b1892
Oyatulloh-Isroilov* <oyatullohisroilov5@gmail.com>
2024-04-18 22:26:23 +0500

    money converter

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import CurrencyConverter from "./components/CurrencyConverter";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <CurrencyConverter />
+    </>
+  );
+}
+
+export default App;
